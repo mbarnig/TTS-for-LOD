1fa8cb89807f6c9513da99d344c0111a27678fc3
Marco BARNIG <mbarnig@pt.lu>
2024-07-03 19:26:09 +0200

    Add files via upload

---
+++ scripts/multilingual/test-inference.sh
@@ -0,0 +1,8 @@
+tts --text "He is doing a great job" \
+--model_path D:\multi-lingual-voice\training\vits_lb-July-02-2024_01+53PM-dbf1a08a\best_model.pth \
+--config_path D:\multi-lingual-voice\training\vits_lb-July-02-2024_01+53PM-dbf1a08a\config.json \
+--out_path D:\multi-lingual-voice\output\test-en.wav \
+--language_idx "en" \
+--language_ids_file_path D:\multi-lingual-voice\training\vits_lb-July-02-2024_01+53PM-dbf1a08a\language_ids.json \
+--speaker_idx "Jim" \
+--speakers_file_path D:\multi-lingual-voice\training\vits_lb-July-02-2024_01+53PM-dbf1a08a\speakers.pth
